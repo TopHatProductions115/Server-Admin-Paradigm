08e96e5b4739bb8955efa232800a0fb9c25bc791
TopHatProductions115 <TopHatProductions115@gmail.com>
2014-09-25 17:13:39 -0400

    Edited for Windows version

--- TXP-MC/toggle.php
+++ TXP-MC/toggle.php
@@ -3,14 +3,14 @@
 $method = $_GET["method"];
 
     if($method == "start"){
-        exec("/jdk1.8.0_20/jre/bin/java -jar /TXP-MC/server/minecraft_server.jar");
+        exec("/jdk1.8.0_20/jre/bin/java -jar /TXP-MC/server/minecraft_server.exe");
     }else if($method == "stop"){
 		exec("killall /jdk1.8.0_20/jre/bin/java");
         
     }else if($method == "restart"){
 	    exec("pkill /jdk1.8.0_20/jre/bin/java");
 	    sleep(7);
-		exec("/jdk1.8.0_20/jre/bin/java -jar /TXP-MC/server/minecraft_server.jar");
+		exec("/jdk1.8.0_20/jre/bin/java -jar /TXP-MC/server/minecraft_server.exe");
 	}
 	
 	header("Location: /TXP-MC/index.php");
